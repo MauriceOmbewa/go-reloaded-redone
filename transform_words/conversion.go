e6c44374f85d39a4f5e6d7955e2be29ff77b8e23
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-04-24 15:22:17 +0300

    Update conversion.go

--- transform_words/conversion.go
+++ transform_words/conversion.go
@@ -10,11 +10,11 @@ func Hexadecimal(words []string) []string {
 		word := words[i]
 		if word == "(hex)" {
 			temp := words[i-1]
-			convtemp, err := strconv.ParseInt(temp, 16, 64)
+			convtemp, err := strconv.ParseInt(temp, 16, 64) // converting the number from type string to int as hexadecimal
 			if err != nil {
 				fmt.Println("Error: ", err)
 			}
-			words[i-1] = strconv.FormatInt(convtemp, 10)
+			words[i-1] = strconv.FormatInt(convtemp, 10) // converting the number to type string from type int, as decimal
 			words = append(words[:i], words[i+1:]...)
 		}
 	}
@@ -26,11 +26,11 @@ func Binary(words []string) []string {
 		word := words[i]
 		if word == "(bin)" {
 			temp := words[i-1]
-			convtemp, err := strconv.ParseInt(temp, 2, 64)
+			convtemp, err := strconv.ParseInt(temp, 2, 64) // converting the number from type string to int as binary
 			if err != nil {
 				fmt.Println("Error: ", err)
 			}
-			words[i-1] = strconv.FormatInt(convtemp, 10)
+			words[i-1] = strconv.FormatInt(convtemp, 10) // converting the number from type int to string as decimal
 			words = append(words[:i], words[i+1:]...)
 		}
 	}
