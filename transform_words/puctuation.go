fb8e5eb2d6ac6f53429c1a0ec72fbb37fc4fa9cb
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-04-24 15:23:53 +0300

    Update puctuation.go

--- transform_words/puctuation.go
+++ transform_words/puctuation.go
@@ -3,19 +3,19 @@ package transformation
 func Punctuation(words []string) []string {
 	for i := 0; i < len(words); i++ {
 		if i > 0 && words[i][0] == '.' || words[i][0] == ',' || words[i][0] == '?' || words[i][0] == ':' || words[i][0] == ';' || words[i][0] == '!' {
-			special := true
+			special := true // variable special is true when the whole word consists of special characters only
 			for j := 0; j < len(words[i]); j++ {
 				if words[i][j] == '.' || words[i][j] == ',' || words[i][j] == '!' || words[i][j] == ':' || words[i][j] == ';' {
 					words[i-1] += string(words[i][j])
 				} else {
-					special = false
+					special = false // variable special is false when a part of the word is made up of special characters and another part made up of letters
 					words[i] = words[i][j:]
 					break
 				}
 			}
 			if special {
 				words = append(words[:i], words[i+1:]...)
-				i--
+				i-- // indicates a decrement in i
 			}
 		}
 	}
