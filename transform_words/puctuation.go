8210f268806695cd7a54963e436cfc090c6ec41f
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-04-29 14:12:37 +0300

    Update puctuation.go

--- transform_words/puctuation.go
+++ transform_words/puctuation.go
@@ -5,7 +5,7 @@ func Punctuation(words []string) []string {
 		if i > 0 && words[i][0] == '.' || words[i][0] == ',' || words[i][0] == '?' || words[i][0] == ':' || words[i][0] == ';' || words[i][0] == '!' {
 			special := true // variable special is true when the whole word consists of special characters only
 			for j := 0; j < len(words[i]); j++ {
-				if words[i][j] == '.' || words[i][j] == ',' || words[i][j] == '!' || words[i][j] == ':' || words[i][j] == ';' {
+				if words[i][j] == '.' || words[i][j] == ',' || words[i][j] == '!' || words[i][j] == ':' || words[i][j] == ';' || words[i][j] == '?' {
 					words[i-1] += string(words[i][j])
 				} else {
 					special = false // variable special is false when a part of the word is made up of special characters and another part made up of letters
